300b1e31242deb9f1d2a22f22cfad33debcddf42
Marcus <anymre@outlook.com>
2019-08-13 18:12:53 +0800

    life

---
+++ life/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <grid id="grid"/>
+        <Button type="success" ghost>Start</Button>
+        <Button type="warning" ghost>Stop</Button>
+    </div>
+</template>
+
+<script>
+    import grid from "./components/grid"
+    export default {
+        name: 'app',
+        components: {grid},
+        data() {
+            return {
+                color: null,
+            }
+        },
+        created: function () {
+
+        },
+    }
+</script>
+
+<style>
+    #grid {
+        position: absolute;
+        left: 46%;
+        top: 20%;
+    }
+</style>
---
+++ life/src/components/grid.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="app">
+      <template v-for="(i,j) in life">
+        <row v-bind:key="j">
+          <template v-for="(c,x) in i">
+            <Button :type="c?'info':''" v-bind:key="x"/>
+          </template>
+        </row>
+      </template>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'grid',
+        data() {
+            return {
+                life: [
+                    [0, 0, 0],
+                    [0, 0, 0],
+                    [0, 0, 0],
+                ]
+            }
+        },
+        created: function () {
+        },
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    button {
+        width: 50px;
+        height: 50px;
+    }
+</style>
